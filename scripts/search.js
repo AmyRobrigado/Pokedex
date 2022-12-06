c0c113a0386ad37790445c85eff3804473410605
Amy_Robrigado <ARobrigado@nyc.yearup.org>
2022-12-06 12:19:59 -0500

    Linked search js file, working on search js

--- scripts/search.js
+++ scripts/search.js
@@ -4,7 +4,24 @@ window.onload = function(){
     // populate dropdown
     document.getElementById("pokeSlct") = pokeDropdown();
 
-}
+    // onchange event
+    document.getElementById("pokeSlct").onchange = onchangePokeSelect();
+
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+    .then(response => response.json)
+    .then(data => {
+    for(let i = 0; i < data.length; i++){
+        
+    }
+    });
+}   
 
 function pokeDropdown(){
+
 }
+
+function onchangePokeSelect(){
+
+}
+
+//pokedexDisplay
